234432f9e60e3976ad993c2c34dd515f98971ea2
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2020-04-03 17:20:36 +0100

    Allow admin access other users data

--- H2020.IPMDecisions.UPR.API/Filters/UserAccessingOwnDataResourceFilter.cs
+++ H2020.IPMDecisions.UPR.API/Filters/UserAccessingOwnDataResourceFilter.cs
@@ -18,8 +18,13 @@ namespace H2020.IPMDecisions.UPR.API.Filters
             {
                 context.Result = new UnauthorizedResult();
                 return;
-            }             
-            var userIdFromToken = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            }
+
+            var userRoleFromToken = claimsIdentity.FindFirst(ClaimTypes.Role)?.Value;      
+            if (userRoleFromToken.ToString().ToLower() == "admin")
+                return;            
+
+            var userIdFromToken = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value;            
             if (!Guid.TryParse(userIdFromToken, out var validatedGuid)){
                 context.Result = new BadRequestObjectResult(new { message = "The 'UserId' on token invalid" });
                 return;
@@ -28,7 +33,7 @@ namespace H2020.IPMDecisions.UPR.API.Filters
             {
                 context.Result = new UnauthorizedResult();
                 return;
-            }            
+            }
         }
     }
 }
