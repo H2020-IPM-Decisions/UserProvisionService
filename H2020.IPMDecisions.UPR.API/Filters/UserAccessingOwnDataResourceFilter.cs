79aba0294054d01896dc4e396a54429c022c555d
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2020-04-21 11:44:00 +0100

    Add test project

--- H2020.IPMDecisions.UPR.API/Filters/UserAccessingOwnDataResourceFilter.cs
+++ H2020.IPMDecisions.UPR.API/Filters/UserAccessingOwnDataResourceFilter.cs
@@ -13,27 +13,36 @@ namespace H2020.IPMDecisions.UPR.API.Filters
 
         public void OnActionExecuting(ActionExecutingContext context)
         {
-            var claimsIdentity = context.HttpContext.User.Identity as ClaimsIdentity;
-            if (claimsIdentity == null)
+            try
             {
-                context.Result = new UnauthorizedResult();
-                return;
-            }
+                var claimsIdentity = context.HttpContext.User.Identity as ClaimsIdentity;
+                if (claimsIdentity == null)
+                {
+                    context.Result = new UnauthorizedResult();
+                    return;
+                }
 
-            var userRoleFromToken = claimsIdentity.FindFirst(ClaimTypes.Role)?.Value;      
-            if (userRoleFromToken != null && userRoleFromToken.ToString().ToLower() == "admin")
-                return;            
+                var userRoleFromToken = claimsIdentity.FindFirst(ClaimTypes.Role)?.Value;
+                if (userRoleFromToken != null && userRoleFromToken.ToString().ToLower() == "admin")
+                    return;
 
-            var userIdFromToken = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value;            
-            if (!Guid.TryParse(userIdFromToken, out var validatedGuid)){
-                context.Result = new BadRequestObjectResult(new { message = "The 'UserId' on token invalid" });
-                return;
+                var userIdFromToken = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (!Guid.TryParse(userIdFromToken, out var validatedGuid))
+                {
+                    context.Result = new BadRequestObjectResult(new { message = "The 'UserId' on token invalid" });
+                    return;
+                }
+                if (validatedGuid.ToString() != context.ActionArguments["userId"].ToString())
+                {
+                    context.Result = new UnauthorizedResult();
+                    return;
+                }
             }
-            if (validatedGuid.ToString() != context.ActionArguments["userId"].ToString())
+            catch (Exception ex)
             {
-                context.Result = new UnauthorizedResult();
+                context.Result = new BadRequestObjectResult(new { message = ex.Message.ToString()}); ;
                 return;
-            }
+            }            
         }
     }
 }
---
+++ H2020.IPMDecisions.UPR.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace H2020.IPMDecisions.UPR.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
