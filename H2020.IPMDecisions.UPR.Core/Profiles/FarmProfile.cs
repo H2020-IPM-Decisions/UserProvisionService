fdea392b04f4cddfa247a3ab54f4c86a001dbcdc
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2020-07-16 13:53:21 +0100

    Mapping profiles update

--- H2020.IPMDecisions.UPR.Core/Profiles/FarmProfile.cs
+++ H2020.IPMDecisions.UPR.Core/Profiles/FarmProfile.cs
@@ -1,5 +1,6 @@
 using H2020.IPMDecisions.UPR.Core.Dtos;
 using H2020.IPMDecisions.UPR.Core.Entities;
+using NetTopologySuite.Geometries;
 
 namespace H2020.IPMDecisions.UPR.Core.Profiles
 {
@@ -11,9 +12,17 @@ namespace H2020.IPMDecisions.UPR.Core.Profiles
             CreateMap<Farm, FarmDto>();
 
             // Dtos to Entities
-            CreateMap<FarmForCreationDto, Farm>();
+            CreateMap<FarmForCreationDto, Farm>()
+                .AfterMap((src, dest) =>
+                {
+                    dest.Location = new Point(src.Location.X, src.Location.Y) { SRID = src.Location.SRID };
+                });
 
-            CreateMap<FarmForUpdateDto, Farm>();
+            CreateMap<FarmForUpdateDto, Farm>()
+                .AfterMap((src, dest) =>
+                {
+                    dest.Location = new Point(src.Location.X, src.Location.Y) { SRID = src.Location.SRID };
+                });
 
             // Dtos to Dtos
             CreateMap<FarmForUpdateDto, FarmForCreationDto>();
