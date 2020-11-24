d026c7ee4506882c743f6ec458388666fc48a6a0
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2020-11-24 14:15:35 +0100

    New Dto for farm Dss

--- H2020.IPMDecisions.UPR.Core/Profiles/FieldProfile.cs
+++ H2020.IPMDecisions.UPR.Core/Profiles/FieldProfile.cs
@@ -1,3 +1,4 @@
+using System;
 using H2020.IPMDecisions.UPR.Core.Dtos;
 using H2020.IPMDecisions.UPR.Core.Entities;
 
@@ -17,6 +18,28 @@ namespace H2020.IPMDecisions.UPR.Core.Profiles
             CreateMap<FieldForCreationDto, Field>();
 
             CreateMap<FieldForUpdateDto, Field>();
+
+            CreateMap<FarmDssForCreationDto, Field>()
+            .BeforeMap((src, dest) =>
+            {
+                if (string.IsNullOrEmpty(src.FieldName))
+                {
+                    dest.Name = string.Format("{0}_{1}_{2}", "Generated_Field", src.CropPest.CropEppoCode, src.CropPest.PestEppoCode);
+                }
+                else
+                {
+                    dest.Name = src.FieldName;
+                }
+
+                if (src.FieldId == null || src.FieldId == Guid.Empty)
+                {
+                    dest.Id = Guid.NewGuid();
+                }
+                else
+                {
+                    dest.Id = (Guid)src.FieldId;
+                }
+            });
 
             // Dtos to Dtos
             CreateMap<FieldForUpdateDto, FieldForCreationDto>();
