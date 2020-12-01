e778e9eb73423b80e65483152793178ecf075c34
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2020-12-01 15:18:17 +0100

    update assemblies. last minute DB change

---
+++ H2020.IPMDecisions.UPR.Data/Persistence/Migrations/20201201141501_RemoveIndexFromFarmLocation.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace H2020.IPMDecisions.UPR.Data.Persistence.Migrations
+{
+    public partial class RemoveIndexFromFarmLocation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Farm_Location",
+                table: "Farm");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Farm_Location",
+                table: "Farm",
+                column: "Location");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Farm_Location",
+                table: "Farm");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Farm_Location",
+                table: "Farm",
+                column: "Location",
+                unique: true);
+        }
+    }
+}
