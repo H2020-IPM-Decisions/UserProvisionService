0319fb8492d6fe164ad7493fb1cb13012c515372
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2021-02-23 16:26:02 +0100

    Allow insert files into database

--- H2020.IPMDecisions.UPR.Core/Entities/WeatherDataSource.cs
+++ H2020.IPMDecisions.UPR.Core/Entities/WeatherDataSource.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -8,6 +9,11 @@ namespace H2020.IPMDecisions.UPR.Core.Entities
         [Key]
         public string Id { get; set; }
         public string Name { get; set; }
+        public string Url { get; set; }
+        public string FileExtension { get; set; }
+        public string FileName { get; set; }
+        public byte[] FileData { get; set; }
+        public DateTime? FileUploadedOn { get; set; }
 
         public IList<FarmWeatherDataSource> FarmWeatherDataSources { get; set; }
         public IList<FieldWeatherDataSource> FieldWeatherDataSources { get; set; }
