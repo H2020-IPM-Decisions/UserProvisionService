8573c9896260a5cdf5ba5b0e62fa13d664ccda7b
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2021-04-15 17:30:21 +0200

    Fix text on error message

--- H2020.IPMDecisions.UPR.Core/Dtos/FarmForCreationDto.cs
+++ H2020.IPMDecisions.UPR.Core/Dtos/FarmForCreationDto.cs
@@ -11,10 +11,10 @@ namespace H2020.IPMDecisions.UPR.Core.Dtos
         [Required(ErrorMessage = "Farm location is required")]
         public override CustomPointLocation Location { get => base.Location; set => base.Location = value; }
 
-        [Required(ErrorMessage = "Weather Forecast Data Source required")]
+        [Required(ErrorMessage = "Weather Historical Data Source is required")]
         public WeatherHistoricalForCreationDto WeatherHistoricalDto { get; set; }
 
-        [Required(ErrorMessage = "Weather Historical Data Source required")]
+        [Required(ErrorMessage = "Weather Forecast Data Source is required")]
         public WeatherForecastForCreationDto WeatherForecastDto { get; set; }
     }
 }
