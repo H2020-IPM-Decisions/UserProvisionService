53ee9acbefe0c2fd52fafade851bf71313c35100
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2021-12-21 12:45:05 +0100

    Update logic to accept different current year

--- H2020.IPMDecisions.UPR.BLL/Helpers/JsonHelper.cs
+++ H2020.IPMDecisions.UPR.BLL/Helpers/JsonHelper.cs
@@ -48,19 +48,21 @@ namespace H2020.IPMDecisions.UPR.BLL.Helpers
             return "";
         }
 
-        public static string AddDefaultDatesToDssJsonInput(string jsonAsString)
+        public static string AddDefaultDatesToDssJsonInput(string jsonAsString, int? currentYear = -1)
         {
+            if (currentYear == -1) currentYear = DateTime.Today.Year;
+
             Regex currentYearRegex = new Regex(@"[{]+[\scurrent_year]+[}]+",
                RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
             if (currentYearRegex.IsMatch(jsonAsString))
             {
-                jsonAsString = currentYearRegex.Replace(jsonAsString, DateTime.Today.Year.ToString());
+                jsonAsString = currentYearRegex.Replace(jsonAsString, currentYear.ToString());
             }
             Regex previousYearRegex = new Regex(@"[{]+[\sprevious_year]+[}]+",
                 RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
             if (previousYearRegex.IsMatch(jsonAsString))
             {
-                jsonAsString = previousYearRegex.Replace(jsonAsString, (DateTime.Today.Year - 1).ToString());
+                jsonAsString = previousYearRegex.Replace(jsonAsString, (currentYear - 1).ToString());
             }
             return jsonAsString;
         }
