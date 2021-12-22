da413af030be4e42c6660723bc5b8050fc22a929
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2021-12-22 15:58:02 +0100

    Weather logic finished

--- H2020.IPMDecisions.UPR.BLL/Helpers/DssDataHelper.cs
+++ H2020.IPMDecisions.UPR.BLL/Helpers/DssDataHelper.cs
@@ -29,25 +29,15 @@ namespace H2020.IPMDecisions.UPR.BLL.Helpers
 
         public static int GetCurrentYearForDssDefaultDates(DssModelInformation dssInputInformation, JObject dssInputSchemaAsJson)
         {
-            // Get last date of weather data
+            var currentYear = -1;
             if (dssInputInformation.Input.WeatherDataPeriodEnd != null)
             {
                 var weatherEndDate = ProcessWeatherDataPeriod(dssInputInformation.Input.WeatherDataPeriodEnd, dssInputSchemaAsJson);
+                currentYear = weatherEndDate.Year;
+                if (DateTime.Today > weatherEndDate) currentYear += 1;
+                return currentYear;
             }
-            // ToDo Get current year based on weather
-            /**
-            The next step will be to ensure that when the model is first selected, that the CURRENT_YEAR is set appropriately.
-            So lets assume that DATE  = the current date (i.e. TODAY),CURRENT_YEAR is year of current date, END_DATE is the last date for which we require weather data & START_DATE is the first date we require weather for – we can then implement the following process:
-            •	Check If DATE > END_DATE 
-                o	If YES - set CURRENT_YEAR = DATE.Year + 1
-                    	Check If DATE < START_DATE
-                        •	If yes - weather data will not currently be available for next season so cannot run model
-                            [we might need to pass a message to UI to indicate that model cannot be run as outside model range]
-                        •	If no – do nothing as we are in new growing season.
-            o	If no – do nothing as CURRENT_YEAR does not need to change
-
-            **/
-            return -1;
+            return currentYear;  
         }
 
         public static DateTime ProcessWeatherDataPeriod(WeatherDataPeriod weatherDataPeriod, JObject dssInputSchemaAsJson, int currentYear = -1)
