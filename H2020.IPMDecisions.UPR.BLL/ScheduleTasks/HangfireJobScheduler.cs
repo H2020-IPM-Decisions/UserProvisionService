06f385ff4824c81d91e2fabf6dab9ca597a50195
antonio-calatayud <antonio.calatayud@adas.co.uk>
2023-03-10 10:10:18 +0100

    Remove 35 from cron job

--- H2020.IPMDecisions.UPR.BLL/ScheduleTasks/HangfireJobScheduler.cs
+++ H2020.IPMDecisions.UPR.BLL/ScheduleTasks/HangfireJobScheduler.cs
@@ -18,7 +18,7 @@ namespace H2020.IPMDecisions.UPR.BLL.ScheduleTasks
 
             RecurringJob.AddOrUpdate<DssRunningJobs>(nameof(DssRunningJobs.ExecuteDssWithErrors),
                 job => job.ExecuteDssWithErrors(JobCancellationToken.Null),
-                "35 */6 * * *", TimeZoneInfo.Utc);
+                "* */6 * * *", TimeZoneInfo.Utc);
         }
     }
 }
